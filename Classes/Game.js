24fd71cb46d2a2a6a70ff0d9e1463aef88e58140
AvivHK <Avivhkn@gmail.com>
2020-03-09 20:54:07 +0200

    done for tomorrow

--- Classes/Game.js
+++ Classes/Game.js
@@ -1,66 +1,66 @@
 class Game {
     constructor(roomId) {
-      this.roomId = roomId;
-      this.board = [];
-      this.moves = 0;
+        this.roomId = roomId;
+        this.board = [];
+        this.moves = 0;
     }
 
     createGameBoard() {
-      function tileClickHandler() {
-        const row = parseInt(this.id.split('_')[1][0], 10);
-        const col = parseInt(this.id.split('_')[1][1], 10);
-        if (!player.getCurrentTurn() || !game) {
-          alert('Its not your turn!');
-          return;
-        }
-        if ($(this).prop('disabled')) {
-          alert('This tile has already been played on!');
-          return;
+        function tileClickHandler() {
+            const row = parseInt(this.id.split('_')[1][0], 10);
+            const col = parseInt(this.id.split('_')[1][1], 10);
+            if (!player.getCurrentTurn() || !game) {
+                alert('Its not your turn!');
+                return;
+            }
+            if ($(this).prop('disabled')) {
+                alert('This tile has already been played on!');
+                return;
+            }
+
+            game.playTurn(this);
+            game.updateBoard(player.getPlayerType(), row, col, this.id);
+
+            player.setCurrentTurn(false);
+            player.updatePlaysArr(1 << ((row * 3) + col));
         }
 
-        game.playTurn(this);
-        game.updateBoard(player.getPlayerType(), row, col, this.id);
-
-        player.setCurrentTurn(false);
-        player.updatePlaysArr(1 << ((row * 3) + col));
-      }
-
-      for (let i = 0; i < 3; i++) {
-        this.board.push(['', '', '']);
-        for (let j = 0; j < 3; j++) {
-          $(`#button_${i}${j}`).on('click', tileClickHandler);
+        for (let i = 0; i < 3; i++) {
+            this.board.push(['', '', '']);
+            for (let j = 0; j < 3; j++) {
+                $(`#button_${i}${j}`).on('click', tileClickHandler);
+            }
         }
-      }
     }
 
     displayBoard(message) {
-      $('.menu').css('display', 'none');
-      $('.gameBoard').css('display', 'block');
-      $('#userHello').html(message);
-      this.createGameBoard();
+        $('.menu').css('display', 'none');
+        $('.gameBoard').css('display', 'block');
+        $('#userHello').html(message);
+        this.createGameBoard();
     }
 
     updateBoard(type, row, col, tile) {
-      $(`#${tile}`).text(type).prop('disabled', true);
-      this.board[row][col] = type;
-      this.moves++;
+        $(`#${tile}`).text(type).prop('disabled', true);
+        this.board[row][col] = type;
+        this.moves++;
     }
 
     getRoomId() {
-      return this.roomId;
+        return this.roomId;
     }
 
     playTurn(tile) {
-      const clickedTile = $(tile).attr('id');
+        const clickedTile = $(tile).attr('id');
 
-      socket.emit('playTurn', {
-        tile: clickedTile,
-        room: this.getRoomId(),
-      });
+        socket.emit('playTurn', {
+            tile: clickedTile,
+            room: this.getRoomId(),
+        });
     }
-    
+
     endGame(message) {
-      alert(message);
-      location.reload();
+        alert(message);
+        location.reload();
     }
-  }
+}
--- Classes/Player.js
+++ Classes/Player.js
@@ -1,35 +1,35 @@
 class Player {
-constructor(name, type) {
-      this.name = name;
-      this.type = type;
-      this.currentTurn = true;
-      this.playsArr = 0;
-    }
-
-    updatePlaysArr(tileValue) {
-      this.playsArr += tileValue;
-    }
-
-    getPlaysArr() {
-      return this.playsArr;
-    }
-
-    setCurrentTurn(turn) {
-      this.currentTurn = turn;
-      const message = turn ? 'Your turn' : 'Waiting for Opponent';
-      $('#turn').text(message);
-    }
-
-    getPlayerName() {
-      return this.name;
-    }
-
-    getPlayerType() {
-      return this.type;
-    }
-
-    getCurrentTurn() {
-      return this.currentTurn;
-    }
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    this.currentTurn = true;
+    this.playsArr = 0;
   }
+
+  updatePlaysArr(tileValue) {
+    this.playsArr += tileValue;
+  }
+
+  getPlaysArr() {
+    return this.playsArr;
+  }
+
+  setCurrentTurn(turn) {
+    this.currentTurn = turn;
+    const message = turn ? 'Your turn' : 'Waiting for Opponent';
+    $('#turn').text(message);
+  }
+
+  getPlayerName() {
+    return this.name;
+  }
+
+  getPlayerType() {
+    return this.type;
+  }
+
+  getCurrentTurn() {
+    return this.currentTurn;
+  }
+}
 
