46228538e929df9ba3b6a71b382c32c176f3e3a7
AvivHK <Avivhkn@gmail.com>
2020-03-10 14:02:33 +0200

    without solveing

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function init() {
   const P1 = 'X';
   const P2 = 'O';
@@ -5,7 +6,6 @@ function init() {
   let game;
 
   const socket = io.connect('http://localhost:3000');
-
 
   $('#new').on('click', () => {
     const name = $('#nameNew').val();
@@ -47,7 +47,6 @@ function init() {
 
   socket.on('player2', (data) => {
     const message = `Hello, ${data.name}`;
-
     game = new Game(data.room);
     game.displayBoard(message);
     player.setCurrentTurn(false);
@@ -56,21 +55,80 @@ function init() {
 
   socket.on('turnPlayed', (data) => {
     const row = data.tile.split('_')[1][0];
+    console.log(row)
     const col = data.tile.split('_')[1][1];
+    console.log(col)
     const opponentType = player.getPlayerType() === P1 ? P2 : P1;
 
     game.updateBoard(opponentType, row, col, data.tile);
     player.setCurrentTurn(true);
   });
 
-  socket.on('gameEnd', (data) => {
-    game.endGame(data.message);
-    socket.leave(data.room);
-  });
-
   socket.on('err', (data) => {
     game.endGame(data.message);
   });
+
+  class Game {
+    constructor(roomId) {
+      this.roomId = roomId;
+      this.board = [];
+    }
+
+    createGameBoard() {
+      function tileClickHandler() {
+        const row = parseInt(this.id.split('_')[1][0], 10);
+        const col = parseInt(this.id.split('_')[1][1], 10);
+        if (!player.getCurrentTurn() || !game) {
+          alert('Its not your turn!');
+          return;
+        }
+
+        game.playTurn(this);
+        game.updateBoard(player.getPlayerType(), row, col, this.id);
+
+        player.setCurrentTurn(false);
+        player.updatePlaysArr(1 << ((row * 3) + col));
+      }
+
+      for (let i = 0; i < 3; i++) {
+        this.board.push(['', '', '']);
+        for (let j = 0; j < 3; j++) {
+          $(`#button_${i}${j}`).on('click', tileClickHandler);
+        }
+      }
+    }
+
+    displayBoard(message) {
+      $('.menu').css('display', 'none');
+      $('.gameBoard').css('display', 'block');
+      $('#userHello').html(message);
+      this.createGameBoard();
+    }
+
+    updateBoard(type, row, col, tile) {
+      $(`#${tile}`).text(type).prop('disabled', true);
+      this.board[row][col] = type;
+    }
+
+    getRoomId() {
+      return this.roomId;
+    }
+
+    playTurn(tile) {
+      const clickedTile = $(tile).attr('id');
+
+      socket.emit('playTurn', {
+        tile: clickedTile,
+        room: this.getRoomId(),
+      });
+    }
+
+    endGame(message) {
+      alert(message);
+      location.reload();
+    }
+  }
+
 };
 
 init()
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const port = 3000
 
 let rooms = 0;
 
@@ -13,17 +14,20 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected!'); 
+    console.log("Player Connected!")
     socket.on('createGame', (data) => {
         socket.join(`${++rooms}`);
         socket.emit('newGame', { name: data.name, room: `${rooms}` });
+        console.log("** Room " + rooms + " created by " + data.name + " **" )
     });
+
     socket.on('joinGame', function (data) {
         let room = io.nsps['/'].adapter.rooms[data.room];
         if (room && room.length === 1) {
             socket.join(data.room);
             socket.broadcast.to(data.room).emit('player1', {});
             socket.emit('player2', { name: data.name, room: data.room })
+            console.log("-- " +  data.name + " is connected to room: " + data.room+ " --")
         } else {
             socket.emit('err', { message: 'Sorry, The room is full!' });
         }
@@ -37,10 +41,8 @@ io.on('connection', (socket) => {
         });
     });
 
-
-    socket.on('gameEnded', (data) => {
-        socket.broadcast.to(data.room).emit('gameEnd', data);
-    });
 });
 
-server.listen(process.env.PORT || 3000);
+let server = app.listen(process.env.PORT || port, function(){
+    console.log('server running on port ' + port)
+});
